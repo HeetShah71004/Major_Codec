2e838755a8949f7d140ea0c603948935fb682e0e
Brad Traversy <me@bradTraversy.com>
2025-08-28 18:31:26 +0530

    codec finish

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ const app = express();
 
 // CORS Setup
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
+  origin: ['https://major-codec-lypu.vercel.app', 'http://localhost:5173'],
   credentials: true,
 }));
 
